aab6244d6ef13bd6d5c024282be82fee5d1159dc
Abbk1109 <abbk1983@gmail.com>
2022-09-14 16:44:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,11 +3,12 @@
 /**
 * times_table - prints the 9 times table, starting with 0
 *
- * Return: Null, Void
+* Return: Null, Void
 */
 void times_table(void)
 {
 	int mm, nn, mul;
+
 	for (mm = 0; mm <= 9; mm++)
 	{
 		for (nn = 0; nn <= 9; nn++)

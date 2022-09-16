9220d888044bd5fbee0a152032e320a4ebcc553c
Abbk1109 <abbk1983@gmail.com>
2022-09-16 09:29:30 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,16 +16,16 @@ for (len = 0; len < n; len++)
 {
 for (space = 0; space < len; space++)
 {
-_putchar(' ');
+putchar('\\');
 }
 
-_putchar('\\');
+putchar(' ');
 if (len == (n - 1))
 {
 continue;
 }
-_putchar('\n');
+putchar('\n');
 }
 }
-_putchar('\n');
+putchar('\n');
 }

4cce7a17994a386565d1a5f1230bfec05a451877
Abbk1109 <abbk1983@gmail.com>
2022-09-17 03:18:25 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,49 +1,34 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
- * @c: third intege
+ * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	 int largest;
 
-	if(a >= b && a >= c)
-	{
-		if(b >= c)
-		{
-			printf("\n\n%.2lf is the 2nd largest number\n", b);
-		}
-		else
-		{
-			printf("\n\n%.2lf is the 2nd largest number\n", c);
+	  if (a > b && a > c)
+	  {
+		  largest = a;
+	  }
+	  else if (a > b && c > a)
+	  {
+		   largest = c;
+	  }
+	   else if (b > c)
+	   {
+		    largest = b;
+	   }
+	    else
+	    {
+		    largest = c;
+	    }
 
-		}
-	}
-	else if(b >= a && b >= c)
-	{
-		if(a >= c)
-		{
-			printf("\n\n%.2lf is the 2nd largest number\n",a);
-		}
-		else
-		{
-			 printf("\n\n%.2lf is the 2nd largest number\n",c);
-		}
-	}
-	else if(a >= b)
-	{
-		printf("\n\n%.2lf is the 2nd largest number\n", a);
-		else
-		{
-			printf("\n\n%.2lf is the 2nd largest number\n", b);
-	}
-
-	 printf("\n\n\t\t\tCoding is Fun !\n\n\n");
-	return 0;
+	    return (largest);
 }
 

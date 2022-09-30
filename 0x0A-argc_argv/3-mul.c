0ca247e073adc1cfe5a97819aab582bce2b4021f
Abbk1109 <abbk1983@gmail.com>
2022-09-30 08:26:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,27 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two arguments and prints result
+ * main - multiplies two numbers
  * @argc: argument count
- * @argv: argument vector
- * Return: 0 if no errors
+ * @argv: arguments
+ * 
+ * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, first_num, sec_num, product;
+  int x, y;
 
-	if (argc != 3)
-	{
-		return (printf("Erro\n"), 1);
-	}
-	for (i = 0; i < argc; i++)
-	{
-		first_num = atoi(argv[1]);
-		sec_num = atoi(argv[2]);
-	}
-	product = first_num * sec_num;
-	printf("%d\n", product);
-	return (0);
+  if (argc < 3)
+    {
+      printf("Error\n");
+      return (1);
+    }
+
+  x = atoi(argv[1]);
+  y = atoi(argv[2]);
+  printf("%d\n", x * y);
+
+  return (0);
 }

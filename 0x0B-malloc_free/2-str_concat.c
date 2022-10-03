509a65a3cf659288845b92e29775dc2df93fc326
Abbk1109 <abbk1983@gmail.com>
2022-10-03 08:54:21 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,49 @@
-#include "main.h"
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _strlen(const char *s);
 
 /**
  * str_concat - concatenates two strings
- * @s1: first input string
- * @s2: second input string
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
  *
- * Return: returns NULL on failure
+ * Return: pointer to the concatenated string
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int s1_len, s2_len, i, j;
-	char *str;
+	int i = 0, j = 0;
+	char *output;
 
 	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		continue;
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		continue;
-	str = malloc(s1_len + s2_len + 1);
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+	i = _strlen(s1);
+	j = _strlen(s2);
 
-	if (str == NULL)
-	{
+	output = malloc((i + j) * sizeof(*s1) + 1);
+
+	if (output == 0)
 		return (NULL);
-	}
-	i = 0;
+	strcat(output, s1);
+	strcat(output, s2);
+	return (output);
+}
 
-	while (i < s1_len)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
+/**
+ * _strlen - gets the length of a string
+ * @s: the string of which length is required
+ *
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
 
-	while (i < (s1_len + s2_len))
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
-	return (str);
+	for (; s[i] != '\0'; i++)
+		;
+	return (i);
 }

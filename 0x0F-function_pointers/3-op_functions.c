6774cd5253c83d8c5a03f4e77226b792d6e71108
Abbk1109 <abbk1983@gmail.com>
2022-10-13 07:25:02 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,61 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two integers
- * @a: first integer
- * @b: second integer
- * Return: sum of two integers
+ * op_add - Add two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The sum of a and b
  */
-
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - subtracts two integers
- * @a: first integer
- * @b: second integer
- * Return: difference of two integers
+ * op_sub - Finds the difference of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The diff of a and b
  */
-
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - multiply two integers
- * @a: first integer
- * @b: second integer
- * Return: product of two integers
+ * op_mul - Multiplies two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The product of a and b
  */
-
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - divides two integers
- * @a: first integer
- * @b: second integer
- * Return: quotient of two integers
+ * op_div - Divides two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The quotient of a and b
  */
-
 int op_div(int a, int b)
 {
-if (!b)
-{
-printf("Error\n");
-exit(100);
-}
-return (a / b);
+	return (a / b);
 }
 
 /**
- * op_mod - calculates modulus two integers
- * @a: first integer
- * @b: second integer
- * Return: modulus of two integers
+ * op_mod - Finds the modulus of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The modulus of a and b
  */
-
 int op_mod(int a, int b)
 {
-if (!b)
-{
-printf("Error\n");
-exit(100);
-}
-return (a % b);
+	return (a % b);
 }

